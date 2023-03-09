b71f82896f65d15fbaef7567926c44ec944f593c
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2023-03-08 22:48:49 -0800

    added basic files. good simple working version for ec2 info and create instance.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# TODO: Improve this setup script a lot. See the project README.md for better info.
+
+# Prior to this:
+# Install the latest Python into your Pyenv: pyenv install 3.11.2
+# pyenv virtualenv 3.11.2 ve.ansible
+# Also, create .python-version in project root, containing: ve.ansible
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install --upgrade wheel
+
