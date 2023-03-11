486cd5fae538c022735c65eb17c778f43e29b757
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2023-03-10 23:09:24 -0800

    refactored, adding input-prompting run shell scripts. playbooks more abstracted and generally improved.

---
+++ run-play01--full-info.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+echo
+echo "COLLECT FULL INFO FOR HOSTS."
+echo "Enter the host or group value as it is configured in your inventory file."
+echo "Enter 'hosts' value: "
+read -r hosts
+
+ansible-playbook -i inventory.yaml play01--full-info.yaml \
+  --extra-vars="hosts=$hosts"
+
---
+++ run-play02--new-ec2-micro.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+echo
+echo "CREATE A NEW EC2 INSTANCE."
+echo "Enter the name to assign your new EC2 instance."
+echo "Enter 'new_instance_name' value: "
+read -r new_instance_name
+
+echo
+echo "Enter the subnet id of the subnet into which you wish to create your new EC2 instance."
+echo "Enter 'new_instance_subnet_id' value: "
+read -r new_instance_subnet_id
+
+ansible-playbook -i inventory.yaml play02--new-ec2-micro.yaml \
+  --extra-vars="new_instance_name=$new_instance_name new_instance_subnet_id=$new_instance_subnet_id"
+
